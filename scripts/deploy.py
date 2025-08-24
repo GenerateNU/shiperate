8b2b35b4f48ff7e51878bda3ea77bd13e19be431
Stone Liu <liu.sto@northeastern.edu>
2025-08-24 16:29:30 -0400

    Add image build and docker python dependency

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,9 @@
 import os
 import shutil
+import docker
 
 REQUIRED_DEPS = ["doctl", "docker"]
-REQUIRED_ENV_VARS = ["DO_TOKEN"]
+REQUIRED_ENV_VARS = ["DO_TOKEN", "DOCKERFILE", "CONTEXT"]
 
 
 def exists(program: str) -> bool:
@@ -21,9 +22,24 @@ def check_dependecies() -> None:
             raise RuntimeError(f"Missing environment variable: {env_var}")
 
 
+def containerize(client: docker.DockerClient) -> None:
+    image, _ = client.images.build(
+        path=os.environ.get("CONTEXT"),
+        dockerfile=os.environ.get("DOCKERFILE"),
+        tag="latest",
+    )
+    print("Successfully built the latest image: ", image)
+
+
 def main():
+    # First verify we have all the tools necessary to deploy to digital ocean.
     check_dependecies()
     print("All dependencies have been verified!")
+    # Build the image from the docker client
+    client = docker.from_env()
+    containerize(client)
+
+    # Second interact with the digital ocean REST API to ensure we have routed the proper containers.
 
 
 if __name__ == "__main__":

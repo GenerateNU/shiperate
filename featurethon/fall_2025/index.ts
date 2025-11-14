281135400c6fca9a74074e6d61a19d99edf8bc18
Stone Liu <liu.sto@northeastern.edu>
2025-11-14 16:37:16 -0500

    Testing logic

--- featurethon/fall_2025/index.ts
+++ featurethon/fall_2025/index.ts
@@ -1,9 +1,23 @@
 import * as dotenv from "dotenv"
 
+type MappingType = Map<string, {
+  messages: string[],
+  addedFiles: Set<string>
+  removedFiles: Set<string>
+  modifiedFiles: Set<string>
+  numberOfCommits: number
+  teamName: string
+}>
+
 const VALID_REPOS = ["prisere", "shiperate", "cinecircle", "specialstandard",
   "karp-backend", "karp-frontend-react", "karp-frontend-react-native"]
 
 const TARGET_BRANCH = "featurethon"
+
+const REPO_TEAM_MAPPINGS: Record<string, string> = {
+  "prisere": "Prisere", "shiperate": "Chiefs", "cinecircle": "CineCircle", "specialstandard": "Special Standard",
+  "karp-backend": "Karp", "karp-frontend-react": "Karp", "karp-frontend-react-native": "Karp"
+}
 
 function validate_github_repos(json: any) {
   const repository_name = json.repository.name
@@ -11,11 +25,53 @@ function validate_github_repos(json: any) {
   return VALID_REPOS.includes(repository_name) && branch_name.startsWith(TARGET_BRANCH)
 }
 
+function process_featurethon(json: any): MappingType {
+  const commits: any[] = json.commits
+  // Create a mapping of a name
+  const mappings = new Map<string, {
+    messages: string[],
+    addedFiles: Set<string>
+    removedFiles: Set<string>
+    modifiedFiles: Set<string>
+    numberOfCommits: number
+    teamName: string
+  }>()
+
+  for (const commit of commits) {
+    const author = commit.author
+    const name = author.name
+    const message: string = commit.message
+    const addedFiles: string[] = commit.added
+    const removedFiles: string[] = commit.removed
+    const modifiedFiles: string[] = commit.modified
+
+    if (mappings.has(name)) {
+      const info = mappings.get(name)!
+      info.messages.push(message)
+      info.addedFiles.union(new Set(addedFiles))
+      info.removedFiles.union(new Set(removedFiles))
+      info.modifiedFiles.union(new Set(modifiedFiles))
+      info.numberOfCommits = info.numberOfCommits + 1
+    } else {
+      mappings.set(name, {
+        messages: [message],
+        addedFiles: new Set<string>(addedFiles),
+        removedFiles: new Set<string>(removedFiles),
+        modifiedFiles: new Set<string>(modifiedFiles),
+        numberOfCommits: 1,
+        teamName: REPO_TEAM_MAPPINGS[json.repository.name]!
+      })
+    }
+  }
+  return mappings
+}
+
 async function handleIncomingGithubWebhook(req: Request): Promise<Response> {
   const json: any = await req.json()
-  await Bun.write("output.txt", JSON.stringify(json));
+  await Bun.write("output.txt", JSON.stringify(json, null, 2));
   if (validate_github_repos(json)) {
-    console.log("Should be valid")
+    const contributors = process_featurethon(json)
+    console.log(contributors)
   }
   return new Response("Successfully Hit Response")
 }
